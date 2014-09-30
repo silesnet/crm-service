deb969e4c36de49632adbd48b71de7afe0d85626
der3k <sikora@silesnet.cz>
2014-09-30 07:31:03 +0200

    added outline of customer form test

---
+++ src/test/groovy/net/snet/crm/service/resources/CustomerFormTest.groovy
@@ -0,0 +1,28 @@
+package net.snet.crm.service.resources
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.common.io.Resources
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * Created by admin on 30.9.14.
+ */
+class CustomerFormTest extends Specification {
+	@Shared ObjectMapper om = new ObjectMapper();
+
+	@Subject CustomerForm customerForm = new CustomerForm(customerFormDataSample())
+
+	def 'it should map draft customer form data into customer table update'() {
+		given: 'draft customer form data'
+		when:
+			def customerUpdate = customerForm.customerUpdate()
+		then:
+			customerUpdate.name == 'Milan Kolouch'
+	}
+
+	Map<String, Object> customerFormDataSample() {
+		om.readValue(Resources.getResource('draft-data-sample.json').text, Map.class).customer
+	}
+}
