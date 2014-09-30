db16730b2330dfd8a9cfba2242fbad248dda3c83
der3k <sikora@silesnet.cz>
2014-09-30 07:31:03 +0200

    added outline of customer form test

--- src/main/java/net/snet/crm/service/resources/CustomerForm.java
+++ src/main/java/net/snet/crm/service/resources/CustomerForm.java
@@ -8,6 +8,23 @@ import java.util.Map;
  * Created by admin on 27.9.14.
  */
 public class CustomerForm {
+	private static final Map<String, String> FIELDS;
+
+	static {
+		FIELDS = Maps.newLinkedHashMap();
+		FIELDS.put("name", "name");
+		FIELDS.put("supplementary_name", "supplementary_name");
+		FIELDS.put("public_id", "public_id");
+		FIELDS.put("dic", "dic");
+		FIELDS.put("representative", "contact_name");
+		FIELDS.put("email", "email");
+		FIELDS.put("email", "email");
+		FIELDS.put("email", "email");
+		FIELDS.put("email", "email");
+		FIELDS.put("email", "email");
+	}
+
+
 	public CustomerForm(Map<String, Object> formMap) {
 		// TODO: implement
 	}
