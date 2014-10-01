566de33a15d28e19caae2af29aa3f8b7ae1675f8
der3k <sikora@silesnet.cz>
2014-10-01 09:16:54 +0200

    started implemention of customer form to customer update mapping

--- src/main/java/net/snet/crm/service/resources/CustomerForm.java
+++ src/main/java/net/snet/crm/service/resources/CustomerForm.java
@@ -1,36 +1,62 @@
 package net.snet.crm.service.resources;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by admin on 27.9.14.
  */
 public class CustomerForm {
-	private static final Map<String, String> FIELDS;
+	private static final Map<String, String> COPY_NOT_EMPTY_FIELDS;
 
 	static {
-		FIELDS = Maps.newLinkedHashMap();
-		FIELDS.put("name", "name");
-		FIELDS.put("supplementary_name", "supplementary_name");
-		FIELDS.put("public_id", "public_id");
-		FIELDS.put("dic", "dic");
-		FIELDS.put("representative", "contact_name");
-		FIELDS.put("email", "email");
-		FIELDS.put("email", "email");
-		FIELDS.put("email", "email");
-		FIELDS.put("email", "email");
-		FIELDS.put("email", "email");
+		COPY_NOT_EMPTY_FIELDS = Maps.newLinkedHashMap();
+		COPY_NOT_EMPTY_FIELDS.put("supplementary_name", "supplementary_name");
+		COPY_NOT_EMPTY_FIELDS.put("public_id", "public_id");
+		COPY_NOT_EMPTY_FIELDS.put("dic", "dic");
+		COPY_NOT_EMPTY_FIELDS.put("contact_name", "contact_name");
+		COPY_NOT_EMPTY_FIELDS.put("email", "email");
+		COPY_NOT_EMPTY_FIELDS.put("phone", "phone");
+		COPY_NOT_EMPTY_FIELDS.put("info", "info");
+		COPY_NOT_EMPTY_FIELDS.put("address.street", "street");
 	}
 
+	private final Map<String, Object> formMap;
 
 	public CustomerForm(Map<String, Object> formMap) {
-		// TODO: implement
+		this.formMap = formMap;
 	}
 
 	public Map<String, Object> customerUpdate() {
-		// TODO: implement
-		return Maps.newHashMap();
+		final HashMap<String, Object> update = Maps.newHashMap();
+		for (Map.Entry<String, String> field : COPY_NOT_EMPTY_FIELDS.entrySet()) {
+			final Optional<Object> value = getNested(field.getKey());
+			if (value.isPresent()) {
+				update.put(field.getValue(), value.get());
+			}
+		}
+		return update;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Optional<Object> getNested(String path) {
+		Object value = formMap;
+		try {
+			for (String key : Splitter.on('.').split(path)) {
+				value = ((Map<String, Object>) value).get(key);
+			}
+		} catch (Exception e) {
+			return Optional.absent();
+		}
+		if (value != null && !"".equals(value.toString().trim())) {
+			return Optional.of(value);
+		} else {
+			return Optional.absent();
+		}
 	}
 }
