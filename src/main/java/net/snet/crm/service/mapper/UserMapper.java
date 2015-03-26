da90e346c52bdd831cf01d10161d4156dc5d16fa
der3k <sikora@silesnet.cz>
2015-03-26 07:35:24 +0100

    add full name to users list

--- src/main/java/net/snet/crm/service/mapper/UserMapper.java
+++ src/main/java/net/snet/crm/service/mapper/UserMapper.java
@@ -9,10 +9,12 @@ import java.sql.SQLException;
 
 public class UserMapper implements ResultSetMapper<User> {
 
-    @Override
-    public User map(int index, ResultSet r, StatementContext ctx) throws SQLException {
-        return new User(r.getLong("id"),
-                r.getString("name"),
-                r.getString("login"));
-    }
+  @Override
+  public User map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+    return new User(
+        r.getLong("id"),
+        r.getString("name"),
+        r.getString("login"),
+        r.getString("full_name"));
+  }
 }
