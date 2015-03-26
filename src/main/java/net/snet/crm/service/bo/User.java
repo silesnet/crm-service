f8f0eec81c6e53147a0d62059d7bf78b09eada3f
der3k <sikora@silesnet.cz>
2015-03-26 07:35:24 +0100

    add full name to users list

--- src/main/java/net/snet/crm/service/bo/User.java
+++ src/main/java/net/snet/crm/service/bo/User.java
@@ -1,37 +1,47 @@
 package net.snet.crm.service.bo;
 
 public class User {
-    private long id;
-    private String login;
-    private String name;
+  private long id;
+  private String login;
+  private String name;
+  private String fullName;
 
-    public User(long id, String name, String login) {
-        this.id = id;
-        this.login = login;
-        this.name = name;
-    }
+  public User(long id, String name, String login, String fullName) {
+    this.id = id;
+    this.login = login;
+    this.name = name;
+    this.fullName = fullName;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getLogin() {
-        return login;
-    }
+  public String getLogin() {
+    return login;
+  }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+  public void setLogin(String login) {
+    this.login = login;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
 }
