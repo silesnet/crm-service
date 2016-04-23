058b440382448a85752b1fc3b4c2385e772d5630
Richard Sikora <sikora@silesnet.cz>
2016-04-23 22:22:19 +0200

    Add network service skeleton

--- src/main/java/net/snet/crm/infrastructure/network/DefaultNetworkService.java
+++ src/main/java/net/snet/crm/infrastructure/network/DefaultNetworkService.java
@@ -4,9 +4,9 @@ import net.snet.crm.domain.model.network.NetworkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SnmpNetworkService implements NetworkService {
+public class DefaultNetworkService implements NetworkService {
 
-  private static final Logger logger = LoggerFactory.getLogger(SnmpNetworkService.class);
+  private static final Logger logger = LoggerFactory.getLogger(DefaultNetworkService.class);
 
   @Override
   public void enableSwitchPort(final String switchName, final int port) {
@@ -36,4 +36,15 @@ public class SnmpNetworkService implements NetworkService {
   public boolean isSwitchPortEnabled(final String switchName, final int port) {
     return false;
   }
+
+  @Override
+  public void kickPppoeUser(final String master, final String login) {
+    final Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        logger.info("'{}' kicked '{}'", master, login);
+      }
+    };
+    final Thread thread = new Thread(task);
+    thread.start();  }
 }
