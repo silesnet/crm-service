720aa9fb1f0d788baf15383c8ce26c2c4e822446
Richard Sikora <sikora@silesnet.cz>
2016-04-27 20:43:11 +0200

    Refactor pppoe_services mode and status values

--- src/test/resources/db/pppoe_services.sql
+++ src/test/resources/db/pppoe_services.sql
@@ -16,7 +16,11 @@ AS
 SELECT p.service_id,
        p.login,
        p.password,
-       p.mode,
+       CASE
+          WHEN p.mode='LAN' THEN 0
+          WHEN p.mode='WIRELESS' THEN 1
+          WHEN p.mode='50' THEN 50
+       END AS mode,
        p.master,
        p.mac,
        p.interface,
@@ -25,7 +29,12 @@ SELECT p.service_id,
            WHEN p.ip IS NULL THEN p.ip_class
            ELSE host(p.ip)
        END AS ip,
-       s.status
+       CASE
+          WHEN p.mode='50' THEN 50
+          WHEN s.status='ACTIVE' THEN 1
+          WHEN s.status='SUSPENDED' THEN 3
+          WHEN s.status='DEBTOR' THEN 2
+       END AS status
 FROM pppoe p
 LEFT JOIN service_connections s ON p.service_id = s.service_id
 ORDER BY service_id
