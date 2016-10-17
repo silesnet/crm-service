1712a2b7ef5f512bace7ea5682faa5cdbe18f6c7
silesnet <>
2016-10-17 15:15:53 +0200

    Add service comments SQL

--- src/main/java/net/snet/crm/service/dao/DbiTodoRepository.java
+++ src/main/java/net/snet/crm/service/dao/DbiTodoRepository.java
@@ -1,6 +1,9 @@
 package net.snet.crm.service.dao;
 
 import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.tweak.HandleCallback;
+import org.skife.jdbi.v2.util.LongMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +17,29 @@ public class DbiTodoRepository implements TodoRepository {
   }
 
   @Override
-  public List<Map<String, Object>> findServiceComments(long serviceId) {
-    return new ArrayList<>();
+  public List<Map<String, Object>> findServiceComments(final long serviceId) {
+    return dbi.withHandle(new HandleCallback<List<Map<String, Object>>>() {
+      @Override
+      public List<Map<String, Object>> withHandle(Handle handle) throws Exception {
+        return handle.createQuery(
+            "SELECT * FROM service_comments WHERE customer_id=:customerId ORDER BY date DESC")
+            .bind("customerId", customerId(serviceId))
+            .list();
+      }
+    });
+  }
+
+  private long customerId(final long serviceId) {
+    return dbi.withHandle(new HandleCallback<Long>() {
+      @Override
+      public Long withHandle(Handle handle) throws Exception {
+        final Long customerId = handle.createQuery(
+            "SELECT customer_id FROM services WHERE id=:serviceId")
+            .bind("serviceId", serviceId)
+            .map(LongMapper.FIRST)
+            .first();
+        return customerId == null ? 0 : customerId;
+      }
+    });
   }
 }
