38cc54787da125f2adc9d3128cf5df6b2bad1a06
silesnet <>
2016-12-14 08:59:13 +0100

    Fix command home configuration precedence resolution

--- src/main/java/net/snet/crm/CrmConfiguration.java
+++ src/main/java/net/snet/crm/CrmConfiguration.java
@@ -2,6 +2,8 @@ package net.snet.crm;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 import io.dropwizard.Configuration;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.db.DataSourceFactory;
@@ -12,6 +14,11 @@ import javax.validation.constraints.NotNull;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import static java.lang.System.getProperty;
+import static java.lang.System.getenv;
+import static java.util.Arrays.asList;
 
 public class CrmConfiguration extends Configuration {
 
@@ -58,17 +65,14 @@ public class CrmConfiguration extends Configuration {
 	}
 
   public File getSystemCommandHome() {
-	  String value = systemCommandHome;
-    if (value == null) {
-      value = System.getProperty("sis.command.home");
-    }
-    if (value == null) {
-      value = System.getenv("SIS_COMMAND_HOME");
-    }
+    final String value = Iterables.find(
+        asList(getProperty("sis.command.home"), getenv("SIS_COMMAND_HOME"), systemCommandHome),
+        Predicates.<String>notNull()
+    );
     return new File(value).getAbsoluteFile();
   }
 
   public SmsMessagingConfiguration getSmsMessaging() {
 		return smsMessaging;
-	}
+  }
 }
--- src/main/java/net/snet/crm/service/utils/Utils.java
+++ src/main/java/net/snet/crm/service/utils/Utils.java
@@ -2,21 +2,21 @@ package net.snet.crm.service.utils;
 
 public class Utils {
 
-    public static String replaceChars(String str, String searchChars, String replaceChars) {
-        int replaceCharsLength = replaceChars.length();
-        int strLength = str.length();
-        StringBuilder buf = new StringBuilder(strLength);
-        for (int i = 0; i < strLength; i++) {
-            char ch = str.charAt(i);
-            int index = searchChars.indexOf(ch);
-            if (index >= 0) {
-                if (index < replaceCharsLength) {
-                    buf.append(replaceChars.charAt(index));
-                }
-            } else {
-                buf.append(ch);
-            }
+  public static String replaceChars(String str, String searchChars, String replaceChars) {
+    int replaceCharsLength = replaceChars.length();
+    int strLength = str.length();
+    StringBuilder buf = new StringBuilder(strLength);
+    for (int i = 0; i < strLength; i++) {
+      char ch = str.charAt(i);
+      int index = searchChars.indexOf(ch);
+      if (index >= 0) {
+        if (index < replaceCharsLength) {
+          buf.append(replaceChars.charAt(index));
         }
-        return buf.toString();
+      } else {
+        buf.append(ch);
+      }
     }
+    return buf.toString();
+  }
 }
