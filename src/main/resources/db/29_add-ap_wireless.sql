74e6ce085ca088b01555f40fb5f3805d0fe5b302
silesnet <>
2017-01-05 07:25:37 +0100

    Fix ap_table creation typo

--- src/main/resources/db/29_add-ap_wireless.sql
+++ src/main/resources/db/29_add-ap_wireless.sql
@@ -3,7 +3,7 @@ CREATE TABLE ap_wireless
    id          bigserial NOT NULL,
    name        text      NOT NULL,
    mac         macaddr   NOT NULL,
-   interface   text      NOT NULL,
+   interface   text      NOT NULL
 );
 
 ALTER TABLE ap_wireless
