c0f08fea9d268d5a1cdbe9093d8e2d37cada7983
silesnet <>
2017-01-18 09:37:18 +0100

    Implemented UpdatePppoe access transition action

--- src/main/java/net/snet/crm/infrastructure/network/access/action/UpdatePppoe.java
+++ src/main/java/net/snet/crm/infrastructure/network/access/action/UpdatePppoe.java
@@ -3,6 +3,7 @@ package net.snet.crm.infrastructure.network.access.action;
 import net.snet.crm.domain.model.network.NetworkRepository;
 import net.snet.crm.domain.model.network.NetworkService;
 import net.snet.crm.domain.shared.data.Data;
+import net.snet.crm.infrastructure.network.access.support.PppoeFactory;
 
 public class UpdatePppoe extends BaseAction
 {
@@ -17,16 +18,22 @@ public class UpdatePppoe extends BaseAction
 
   @Override
   boolean initialize() {
-    return super.initialize();
+    final PppoeFactory factory = new PppoeFactory(networkRepository);
+    pppoe = factory.pppoeOf(draft);
+    return !pppoe.isEmpty();
   }
 
   @Override
   void updateDatabase() {
-    super.updateDatabase();
+    networkRepository.updatePppoe(serviceId, pppoe, handle);
+    log.info("updated PPPoE for service '{}'", serviceId);
   }
 
   @Override
   void updateNetwork() {
-    super.updateNetwork();
+    final String master = pppoe.stringOf("master");
+    final String login = pppoe.stringOf("login");
+    networkService.kickPppoeUser(master, login);
+    appendMessage("info: kicked '%s' from '%s'", login, master);
   }
 }
