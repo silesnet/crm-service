04264581444438c623e977689fc44ff6089dbb48
silesnet <richard.sikora@tieto.com>
2017-05-23 17:12:35 +0200

    Remove manual lexems creation of customer name

--- src/main/resources/db/35_add-customer-search-index.sql
+++ src/main/resources/db/35_add-customer-search-index.sql
@@ -5,3 +5,13 @@ UPDATE customers
    SET lexems = to_tsvector(normalize_text(name));
 
 CREATE INDEX customers_search_idx ON customers USING gin(lexems);
+
+CREATE OR REPLACE FUNCTION calculate_customer_lexems () RETURNS TRIGGER AS '
+BEGIN
+  NEW.lexems = to_tsvector(normalize_text(NEW.name));
+  RETURN NEW;
+END;
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER calculate_customer_lexems_trg BEFORE INSERT OR UPDATE
+  ON customers FOR EACH ROW EXECUTE PROCEDURE calculate_customer_lexems ();
