e44304d1c326ad2a9c96781a2d5ade28671ad0aa
silesnet <>
2017-09-18 10:00:02 +0200

    Improve even more customer connection status view

--- src/main/resources/db/41_add-customer-connection-status-view.sql
+++ src/main/resources/db/41_add-customer-connection-status-view.sql
@@ -2,23 +2,29 @@ CREATE OR REPLACE VIEW customer_connection_status
 (
   customer_id,
   service_statuses,
+  has_active,
   active_status,
+  has_suspended,
   suspended_status,
+  has_debtor,
   debtor_status
 )
 AS 
 SELECT s.customer_id
        , JSON_OBJECT(ARRAY_AGG(s.id::TEXT), ARRAY_AGG(c.status)) AS service_statuses
+       , STRING_AGG(c.status,'') ~ 'ACTIVE' AS has_active
        , CASE
          WHEN REPLACE(STRING_AGG(c.status,''),'ACTIVE','') = '' THEN 'ALL_ACTIVE'
          WHEN REPLACE(STRING_AGG(c.status,''),'ACTIVE','') != STRING_AGG(c.status,'') THEN 'SOME_ACTIVE'
          ELSE 'NONE_ACTIVE'
        END AS active_status
+       , STRING_AGG(c.status,'') ~ 'SUSPENDED' AS has_suspended
        , CASE
          WHEN REPLACE(STRING_AGG(c.status,''),'SUSPENDED','') = '' THEN 'ALL_SUSPENDED'
          WHEN REPLACE(STRING_AGG(c.status,''),'SUSPENDED','') != STRING_AGG(c.status,'') THEN 'SOME_SUSPENDED'
          ELSE 'NONE_SUSPENDED'
        END AS suspended_status
+       , STRING_AGG(c.status,'') ~ 'DEBTOR' AS has_debtor
        , CASE
          WHEN REPLACE(STRING_AGG(c.status,''),'DEBTOR','') = '' THEN 'ALL_DEBTOR'
          WHEN REPLACE(STRING_AGG(c.status,''),'DEBTOR','') != STRING_AGG(c.status,'') THEN 'SOME_DEBTOR'
