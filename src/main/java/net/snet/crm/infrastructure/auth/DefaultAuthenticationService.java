15437f17e13148e448db1a861e2f0b8364bc6f57
silesnet <>
2020-01-18 13:53:23 +0100

    Fix session cancelling

--- src/main/java/net/snet/crm/infrastructure/auth/DefaultAuthenticationService.java
+++ src/main/java/net/snet/crm/infrastructure/auth/DefaultAuthenticationService.java
@@ -2,8 +2,10 @@ package net.snet.crm.infrastructure.auth;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Maps;
 import net.snet.crm.service.auth.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 public class DefaultAuthenticationService implements AuthenticationService {
@@ -14,17 +16,20 @@ public class DefaultAuthenticationService implements AuthenticationService {
   private final UserService userService;
 
   private final BiMap<AccessToken, AuthenticatedUser> users;
+  private final Map<SessionId, AccessToken> sessions;
 
   public DefaultAuthenticationService(UserService userService) {
     this.userService = userService;
-    this.users = HashBiMap.create();
+    users = HashBiMap.create();
     users.put(TEST_ACCESS_TOKEN, TEST_USER);
+    sessions = Maps.newHashMap();
+    sessions.put(TEST_SESSION_ID, TEST_ACCESS_TOKEN);
   }
 
   @Override
   public AccessToken authenticate(SessionId sessionId) {
-    if (TEST_SESSION_ID.equals(sessionId)) {
-      return TEST_ACCESS_TOKEN;
+    if (sessions.containsKey(sessionId)) {
+      return sessions.get(sessionId);
     }
     AuthenticatedUser user = userService.authenticate(sessionId);
     return issueAccessToken(user);
