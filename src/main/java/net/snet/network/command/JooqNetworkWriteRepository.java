7415c3d79365f90337cb0298f4f29e1aed8a96c3
silesnet <>
2020-12-01 16:45:57 +0100

    Implement update node record

--- src/main/java/net/snet/network/command/JooqNetworkWriteRepository.java
+++ src/main/java/net/snet/network/command/JooqNetworkWriteRepository.java
@@ -2,10 +2,13 @@ package net.snet.network.command;
 
 import lombok.extern.slf4j.Slf4j;
 import net.snet.crm.infra.db.command.Tables;
+import net.snet.crm.infra.db.command.tables.Network;
 import net.snet.crm.infra.db.command.tables.records.NetworkRecord;
 import net.snet.network.command.domain.model.NetworkWriteRepository;
 import net.snet.network.command.domain.model.Node;
 import org.jooq.DSLContext;
+
+import static net.snet.crm.infra.db.command.tables.Network.NETWORK;
 
 @Slf4j
 public class JooqNetworkWriteRepository implements NetworkWriteRepository {
@@ -30,6 +33,14 @@ public class JooqNetworkWriteRepository implements NetworkWriteRepository {
 
   @Override
   public Node updateNode(Node node) {
-    return null;
+    LOGGER.info("updating node {}", node.toString());
+    final NetworkRecord record = db.fetchOne(NETWORK, NETWORK.ID.eq((Integer) node.getAttributes().get("id")));
+    record.fromMap(node.getAttributes());
+    LOGGER.info("network record before update {}", record);
+    record.store();
+    LOGGER.info("network record after update {}", record);
+    final Node updated = new Node(record.intoMap());
+    LOGGER.info("updated node {}", updated.toString());
+    return updated;
   }
 }
